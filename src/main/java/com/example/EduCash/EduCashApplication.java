73d42c9b861890820fefbc1bf7fa570514a707c7
ErickATE1306 <arnoldantezanatorres@gmail.com>
2025-11-16 10:02:53 -0500

    listo

--- src/main/java/com/example/EduCash/EduCashApplication.java
+++ src/main/java/com/example/EduCash/EduCashApplication.java
@@ -2,8 +2,11 @@ package com.example.EduCash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+// Excluir autoconfiguración de BD/JPA temporalmente para permitir arrancar sin MySQL
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class EduCashApplication {
 
 	public static void main(String[] args) {
